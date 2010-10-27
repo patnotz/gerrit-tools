7d94effb3276dfae322ca2e8adaa4f93f4241133
Pat Notz <pknotz@sandia.gov>
2010-10-27 17:20:12 -0600

    show-gerrit-users.sh: add registered_on date/time

--- show-gerrit-users.sh
+++ show-gerrit-users.sh
@@ -1,1 +1,1 @@
-ssh -p 5915 sierra-trac.sandia.gov 'gerrit gsql --format PRETTY -c "select full_name,preferred_email from accounts"'
+ssh -p 5915 sierra-trac.sandia.gov 'gerrit gsql --format PRETTY -c "select full_name,preferred_email,registered_on from accounts"'
